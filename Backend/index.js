b0044d81ab88fac32e52f62c3bac17d35098a823
SAwandya <sachilaawandya@gmail.com>
2024-02-09 13:17:21 +0530

    create get for products in the routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
+const products = require("./routes/products");
 
 mongoose
-.connect("mongodb://localhost/dairydb")
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log("Could not connect to MongoDB"));
+  .connect("mongodb://localhost/dairydb")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Could not connect to MongoDB"));
 
 app.use(express.json());
+
+app.use("/api/products", products);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
