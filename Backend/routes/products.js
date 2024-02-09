3264eb6ba2842f0b8a70299e4f44be7756c1893c
SAwandya <sachilaawandya@gmail.com>
2024-02-09 17:12:06 +0530

    create route for product posting

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -3,6 +3,29 @@ const router = express.Router();
 const { Product, validate } = require("../models/product");
 
 router.get("/", async (req, res) => {
-    const products = await Product.find.sort("name");
+    const products = await Product.find().sort("name");
     res.send(products);
+});
+
+router.post("/", async (req, res) => {
+
+    const { error } = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      unitOfMeasurement: req.body.unitOfMeasurement,
+      expirationDate: req.body.expirationDate,
+      manufacDtae: req.body.manufacDtae,
+    });
+
+    product = await product.save();
+
+    res.send(product);
 })
+
+module.exports = router;
