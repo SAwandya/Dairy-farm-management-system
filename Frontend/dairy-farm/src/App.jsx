74cf07eae088c0a2182d7d7acaaa4869277df3cf
SAwandya <sachilaawandya@gmail.com>
2024-02-09 18:41:12 +0530

    create card component

--- Frontend/dairy-farm/src/App.jsx
+++ Frontend/dairy-farm/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import NavBar from './components/NavBar'
 import "@fontsource/roboto/400.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import ProductCard from './components/ProductCard';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <CssBaseline/>
       <NavBar/>
+      <ProductCard/>
     </>
   )
 }
