aa089c39a4f697e86c55c63ec7e38aa7941ff9ea
SAwandya <sachilaawandya@gmail.com>
2024-02-10 19:07:49 +0530

    create signup page

--- Frontend/dairy-farm/src/App.jsx
+++ Frontend/dairy-farm/src/App.jsx
@@ -3,14 +3,15 @@ import NavBar from './components/NavBar'
 import "@fontsource/roboto/400.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import ProductGrid from './components/ProductGrid';
+import CustomerSignUp from './components/CustomerSignUp';
 
 function App() {
 
   return (
     <>
       <CssBaseline/>
-      <NavBar/>
-      <ProductGrid/>
+      
+      <CustomerSignUp/>
     </>
   )
 }
