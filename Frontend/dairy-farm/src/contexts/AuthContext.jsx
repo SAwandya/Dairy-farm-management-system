919f882d2af5c31c82ba1fb1915cfff23865fff0
SAwandya <sachilaawandya@gmail.com>
2024-02-15 10:30:21 +0530

    create context

---
+++ Frontend/dairy-farm/src/contexts/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [authToken, setAuthToken] = useState(localStorage.getItem("token"));
+
+  const login = (token) => {
+    setAuthToken(token);
+    localStorage.setItem("token", token);
+  };
+
+  const logout = () => {
+    setAuthToken(null);
+    localStorage.removeItem("token");
+  };
+
+  const getCurrentUser = () => {
+    const userId = jwtDecode(authToken);
+
+    return userId;
+  };
+
+  return (
+    <AuthContext.Provider value={{ authToken, login, logout, getCurrentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
+
