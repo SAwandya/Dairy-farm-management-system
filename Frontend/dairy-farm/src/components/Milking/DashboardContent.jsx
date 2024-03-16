53b01cf32ea691ba647ebc10f0d7c2b889501ed5
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-03-17 03:35:22 +0530

    Create stat cards container

--- Frontend/dairy-farm/src/components/Milking/DashboardContent.jsx
+++ Frontend/dairy-farm/src/components/Milking/DashboardContent.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import Welcome from './Welcome'
+import MainStatistics from './MainStatistics';
 
 function DashboardContent() {
     return (
         <Box className="dashboard-content">
             <Welcome/>
+            <MainStatistics/>
         </Box>
     );
   }
