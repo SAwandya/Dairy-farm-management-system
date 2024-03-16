9fa8ee406891fa32a4ce16a442389456569fb94c
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-03-17 01:12:09 +0530

    Create the home dashboard header

---
+++ Frontend/dairy-farm/src/components/Milking/Date.jsx
@@ -0,0 +1,24 @@
+import { Box, Icon } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
+function Date() {
+    return (
+        <Box className="date-container">
+            <Typography variant="h4" className='date-text'>Today:</Typography>
+            <Box className="calendar-container">
+                <CalendarMonthIcon 
+                    sx={{
+                        width: '40px',
+                        height: '40px',
+                        marginRight: '12px'
+                    }}
+                />
+                <Typography variant="h1" className='date-value'>20/03/2024</Typography>
+            </Box>
+        </Box>
+    );
+  }
+  
+export default Date;
---
+++ Frontend/dairy-farm/src/components/Milking/MainStatistics.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+function MainStatistics() {
+    return (
+        <Box>
+            
+        </Box>
+    );
+}
+
+export default MainStatistics;
