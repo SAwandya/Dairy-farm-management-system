1322db1444d1a82d9f4099c0cdae0d5c65e75953
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-03-17 14:43:33 +0530

    Create the Milking data graph card

--- Frontend/dairy-farm/src/components/Milking/DashboardContent.jsx
+++ Frontend/dairy-farm/src/components/Milking/DashboardContent.jsx
@@ -2,12 +2,18 @@ import { Box } from '@mui/material';
 import React from 'react';
 import Welcome from './Welcome'
 import MainStatistics from './MainStatistics';
+import ChartCard from './ChartCard';
+import TankStausCard from './TankStausCard';
 
 function DashboardContent() {
     return (
         <Box className="dashboard-content">
             <Welcome/>
             <MainStatistics/>
+            <Box className="dashboard-below-contaienr">
+                <ChartCard/>
+                <TankStausCard/>
+            </Box>
         </Box>
     );
   }
---
+++ Frontend/dairy-farm/src/components/Milking/TankStatusCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from '@mui/material';
+
+function TankStausCard() {
+    return (
+        <Box>
+            <Typography></Typography>
+        </Box>
+    );
+  }
+  
+  export default TankStausCard;
