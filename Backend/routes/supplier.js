378752621ef2062263b68d402de9eea5a2a7cf52
Thusala Piyarisi <49372490+Isnoc-tech@users.noreply.github.com>
2024-03-18 22:53:21 +0530

    Supplier backend

---
+++ Backend/routes/supplier.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const { validate, Supplier } = require("../models/supplier");
+const c = require("config");
+const { number } = require("joi");
+
+// Create
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let supplier = new Supplier({
+        name: req.body.name,
+        email: req.body.email,
+        itemType: req.body.itemType,
+        deliveryDate: req.body.deliveryDate
+    });
+    supplier = await supplier.save();
+
+    res.send(supplier);
+});
+
+// Read all
+router.get('/', async (req, res) => {
+    const suppliers = await Supplier.find();
+    res.send(suppliers);
+});
+
+// Read one
+router.get('/:id', async (req, res) => {
+    const supplier = await Supplier.findById(req.params.id);
+    if (!supplier) return res.status(404).send('The supplier with the given ID was not found.');
+    res.send(supplier);
+});
+
+// Update
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const supplier = await Supplier.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        email: req.body.email,
+        itemType: req.body.itemType,
+        deliveryDate: req.body.deliveryDate
+    }, { new: true });
+
+    if (!supplier) return res.status(404).send('The supplier with the given ID was not found.');
+
+    res.send(supplier);
+});
+
+// Delete
+router.delete('/:id', async (req, res) => {
+    const supplier = await Supplier.findByIdAndDelete(req.params.id);
+    if (!supplier) return res.status(404).send('The supplier with the given ID was not found.');
+    res.send(supplier);
+});
+
+module.exports = router;
