71bbd725dd55d1ffe53bf98ebcf58ccde270c34a
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-03-19 00:21:33 +0530

    Sidebar fix

--- Frontend/dairy-farm/src/components/Milking/MilkingSideBar.jsx
+++ Frontend/dairy-farm/src/components/Milking/MilkingSideBar.jsx
@@ -18,7 +18,12 @@ function Sidebar() {
   };
 
   return (
-    <Box className="sidebar">
+    <Box
+      className="sidebar"
+      sx={{
+        position: 'fixed'
+      }}
+    >
       <Box className="sidebar-logo-container">
         <img
           className="sidebar-logo"
