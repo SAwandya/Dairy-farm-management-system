05361fcdd257a1c6bc1245da9af7496c7f290020
SAwandya <sachilaawandya@gmail.com>
2024-04-01 13:16:05 +0530

    create myorder page

---
+++ Frontend/dairy-farm/src/pages/Sales/OrderPage.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import { Container } from '@mui/material';
+
+const OrderPage = () => {
+
+    
+
+  return (
+    <>
+      <Container sx={{ margin: "150px", alignItems: "center" }}>
+        <Accordion sx={{ marginTop: "20px" }} defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+          >
+            Accordion Actions
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+          <AccordionActions>
+            <Button>Cancel</Button>
+            <Button>Agree</Button>
+          </AccordionActions>
+        </Accordion>
+
+        <Accordion sx={{ marginTop: "20px" }} defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+          >
+            Accordion Actions
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+          <AccordionActions>
+            <Button>Cancel</Button>
+            <Button>Agree</Button>
+          </AccordionActions>
+        </Accordion>
+      </Container>
+    </>
+  );
+}
+
+
+export default OrderPage
