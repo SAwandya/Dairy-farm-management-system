3c17a55a9d92099158a032c35c1b019b67da1bf2
Disara Methmali <it22080394@my.sliit.lk>
2024-05-01 19:03:12 +0530

    QR

---
+++ Frontend/dairy-farm/src/pages/Employees/QRCodeScanner.jsx
@@ -0,0 +1,16 @@
+// QRCodeScanner.js
+
+import React from 'react';
+import QRCode from 'qrcode.react';
+
+const QRCodeScanner = () => {
+  return (
+    <div>
+      <h1>Scan QR Code for Attendance</h1>
+      <QRCode value="https://forms.gle/48GRhm7DF4e1ioZk8" />
+      <p>Scan the QR code to access the attendance form.</p>
+    </div>
+  );
+};
+
+export default QRCodeScanner;
