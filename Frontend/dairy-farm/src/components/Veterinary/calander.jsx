689a57022a3f124798afa3d685c10f407281ed39
Duvini Ranaweera <121706197+Duvini@users.noreply.github.com>
2024-05-02 11:35:54 +0530

    Date calender UI added

--- Frontend/dairy-farm/src/components/Veterinary/calander.jsx
+++ Frontend/dairy-farm/src/components/Veterinary/calander.jsx
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, styled } from '@mui/material';
+import { Typography, Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, styled, Badge } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider, PickersDay, DateCalendar } from '@mui/x-date-pickers';
 import CheckIcon from '@mui/icons-material/Check';
 import dayjs from 'dayjs';
-import axios from 'axios'; 
-
+import axios from 'axios';
 
 const HeaderTypography = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(1),
   fontWeight: 'bold',
 }));
 
-
 const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
-  backgroundColor: 'transparent',
+  backgroundColor: '',
+}));
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: '8px',
+    top: '8px',
+  },
+}));
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  fontSize: 15, 
+  fontWeight: 'bold', 
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  fontSize: 15, 
+  fontWeight: 'bold',
 }));
 
 const Calendar = ({ onNextDateData }) => {
@@ -23,8 +37,8 @@ const Calendar = ({ onNextDateData }) => {
   const [highlightedDays, setHighlightedDays] = useState([]);
   const [earTag, setEarTag] = useState([]);
   const [vaccine, setVaccine] = useState([]);
+  const [vacDate, setVacDate] = useState([]);
 
- 
   const fetchDataByDate = async (date) => {
     try {
       const response = await axios.get(`http://localhost:3000/api/vacAnim/date/${date}`);
@@ -38,27 +52,29 @@ const Calendar = ({ onNextDateData }) => {
     return [];
   };
 
-  
   const handleDateSelect = async (newValue) => {
-    setValue(newValue); 
-    const selectedDate = newValue.format('YYYY-MM-DD'); 
+    setValue(newValue);
+    const selectedDate = newValue.format('YYYY-MM-DD');
     const data = await fetchDataByDate(selectedDate);
     onNextDateData(data);
 
     if (data.length > 0) {
       const earTags = data.map(item => item.earTag);
       const vaccines = data.map(item => item.vaccine);
+      const vacDate = data.map(item => item.vacdate); 
       setEarTag(earTags);
       setVaccine(vaccines);
+      setVacDate(vacDate); 
     } else {
       setEarTag(['No Vaccinations']);
       setVaccine(['']);
+      setVacDate(['']);
     }
   };
 
   useEffect(() => {
-    if (Array.isArray(onNextDateData)) {
-      const nextDate = onNextDateData[0]?.date; 
+    if (Array.isArray(onNextDateData) && onNextDateData.length > 0) { 
+      const nextDate = onNextDateData[0]?.date;
       const highlighted = nextDate ? [dayjs(nextDate).date()] : [];
       setHighlightedDays(highlighted);
     }
@@ -66,66 +82,63 @@ const Calendar = ({ onNextDateData }) => {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Box p={2} border={1} borderColor="divider" display="flex">
-        <Box flex="1">
-          <StyledTableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Ear Tag</TableCell>
-                  <TableCell>Vaccine</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {earTag.map((tag, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{tag}</TableCell>
-                    <TableCell>{vaccine[index]}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </StyledTableContainer>
-        </Box>
-        <Box flex="1">
-          <Box p={2}>
-            <HeaderTypography variant="subtitle1" align="center">
-              SELECT DATE
-            </HeaderTypography>
-            <HeaderTypography variant="subtitle1" align="center">
-              Schedules Vaccination for
-            </HeaderTypography>
-            <Typography variant="subtitle1" align="center">
-              {value.format('ddd, MMM D')}
-            </Typography>
-            <Grid container spacing={2} justifyContent="center">
-              <Grid item xs={12} sm={6} md={8}>
-                <DateCalendar
-                  date={value}
-                  onChange={handleDateSelect}
-                  renderDay={(day, _value, DayComponentProps) => {
-                    const isSelected =
-                      !DayComponentProps.outsideCurrentMonth &&
-                      highlightedDays.indexOf(day.getDate()) >= 0;
+      <Grid container justifyContent="center"> 
+        <Grid item xs={12} > 
+          <Box p={2} border={1} borderColor="divider" display="flex" flexDirection="column" >
+            <Box p={2} bgcolor="#f0f0f0" width={'400px'} mx="auto"> 
+              <HeaderTypography variant="subtitle1" align="center">
+                Schedules Vaccination for {value.format('MMMM DD, YYYY')}
+              </HeaderTypography>
+              <Typography variant="subtitle1" align="center">
+                Select Date
+              </Typography>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12}>
+                  <DateCalendar
+                    date={value}
+                    onChange={handleDateSelect}
+                    renderDay={(day, _value, DayComponentProps) => {
+                      const isNextDate = highlightedDays.includes(day.date());
 
-                    const isNextDate = highlightedDays.includes(day.date());
-
-                    return (
-                      <Badge
-                        key={day.toString()}
-                        overlap='circular'
-                        badgeContent={isNextDate ? <CheckIcon color='red' /> : undefined}
-                      >
-                        <PickersDay {...DayComponentProps} />
-                      </Badge>
-                    );
-                  }}
-                />
+                      return (
+                        <StyledBadge
+                          key={day.toString()}
+                          overlap='circular'
+                          badgeContent={isNextDate ? <CheckIcon color='primary' /> : null}
+                        >
+                          <PickersDay {...DayComponentProps} />
+                        </StyledBadge>
+                      );
+                    }}
+                  />
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
+            <Box mt={2}>
+              <StyledTableContainer component={Paper}>
+                <Table>
+                  <TableHead >
+                    <StyledTableRow >
+                      <StyledTableCell>Ear Tag</StyledTableCell>
+                      <StyledTableCell>Vaccine Name</StyledTableCell>
+                      <StyledTableCell>Vaccinated Date</StyledTableCell>
+                    </StyledTableRow>
+                  </TableHead>
+                  <TableBody>
+                    {earTag.map((tag, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{tag}</TableCell>
+                        <TableCell>{vaccine[index]}</TableCell>
+                        <TableCell>{vacDate[index]}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </StyledTableContainer>
+            </Box>
           </Box>
-        </Box>
-      </Box>
+        </Grid>
+      </Grid>
     </LocalizationProvider>
   );
 };
