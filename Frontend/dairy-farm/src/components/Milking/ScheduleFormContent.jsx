df2e30bc0467c20e28af6c208af13ab26d131879
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-05-03 00:51:09 +0530

    Complete frontend validations in session scheduling form

--- Frontend/dairy-farm/src/components/Milking/ScheduleFormContent.jsx
+++ Frontend/dairy-farm/src/components/Milking/ScheduleFormContent.jsx
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { TextField, Button, Box, Typography, Select, MenuItem } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Swal from 'sweetalert2';
 
 const ScheduleFormContent = () => {
     const [date, setDate] = useState('');
+    const [dateError, setDateError] = useState('');
     const [time, setTime] = useState('');
+    const [timeError, setTimeError] = useState('');
     const [cowGroup, setCowGroup] = useState('');
-    const [status, setStatus] = useState('Incomplete');
+    const [cowGroupError, setCowGroupError] = useState('');
     const [specialNotes, setSpecialNotes] = useState('');
+    const [specialNotesError, setSpecialNotesError] = useState('');
     const [batchOptions, setBatchOptions] = useState([]);
+    const [status, setStatus] = useState('Incomplete');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -30,6 +36,18 @@ const ScheduleFormContent = () => {
         }
     };
 
+    const displaySuccessToast = (message) => {
+        toast.success(message, {
+            position: "top-right",
+            autoClose: 2800,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
     
@@ -41,18 +59,85 @@ const ScheduleFormContent = () => {
             specialNotes
         })
         .then(result => {
-            // console.log(result);
-            navigate('/milkingSessions');
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Successfuly added",
-                showConfirmButton: false,
-                timer: 1500
-            });
+            displaySuccessToast('Successfully added!');
+            setTimeout(() => {
+                navigate('/milkingSessions');
+            }, 3000);
         })
         .catch(err => console.log(err));
-        
+    };
+
+    const isFormValid = () => {
+        return !dateError && !timeError && !cowGroupError && !specialNotesError;
+    };
+
+    const validateDate = (value) => {
+        if (!value) {
+            setDateError('Date is required');
+        } else if (!isValidDate(value)) {
+            setDateError('Invalid date format');
+        } else {
+            setDateError('');
+        }
+    };
+
+    const isValidDate = (value) => {
+        const pattern = /^\d{4}-\d{2}-\d{2}$/;
+        return pattern.test(value);
+    };
+
+    const handleDateChange = (value) => {
+        setDate(value);
+        validateDate(value);
+    
+        const enteredDate = new Date(value);
+        const today = new Date();
+        today.setDate(today.getDate() - 1); 
+        if (enteredDate < today) {
+            setDateError('Date cannot be before today');
+        } else {
+            setDateError('');
+        }
+    };
+    
+
+    const validateTime = (value) => {
+        if (!value) {
+            setTimeError('Time is required');
+        } else {
+            setTimeError('');
+        }
+    };
+
+    const handleTimeChange = (value) => {
+        setTime(value);
+        validateTime(value);
+    };
+
+    const validateCowGroup = (value) => {
+        if (!value) {
+            setCowGroupError('Cow Group is required');
+        } else {
+            setCowGroupError('');
+        }
+    };
+
+    const handleCowGroupChange = (value) => {
+        setCowGroup(value);
+        validateCowGroup(value);
+    };
+
+    const validateSpecialNotes = (value) => {
+        if (!value) {
+            setSpecialNotesError('Special Notes are required');
+        } else {
+            setSpecialNotesError('');
+        }
+    };
+
+    const handleSpecialNotesChange = (value) => {
+        setSpecialNotes(value);
+        validateSpecialNotes(value);
     };
 
     return (
@@ -94,10 +179,15 @@ const ScheduleFormContent = () => {
                             InputLabelProps={{
                                 shrink: true,
                             }}
+                            inputProps={{
+                                min: new Date().toISOString().split('T')[0]
+                            }}
                             value={date}
-                            onChange={(e) => setDate(e.target.value)}
+                            onChange={(e) => handleDateChange(e.target.value)}
                             fullWidth
                             required
+                            error={!!dateError}
+                            helperText={dateError}
                             style={{
                                 marginBottom: '16px'
                             }}
@@ -110,9 +200,12 @@ const ScheduleFormContent = () => {
                                 shrink: true,
                             }}
                             value={time}
-                            onChange={(e) => setTime(e.target.value)}
+                            onChange={(e) => handleTimeChange(e.target.value)}
                             fullWidth
                             required
+                            disabled={!date}
+                            error={!!timeError}
+                            helperText={timeError}
                             style={{
                                 marginBottom: '16px'
                             }}
@@ -121,9 +214,12 @@ const ScheduleFormContent = () => {
                             name="cowGroup"
                             label="Cow Group"
                             value={cowGroup}
-                            onChange={(e) => setCowGroup(e.target.value)}
+                            onChange={(e) => handleCowGroupChange(e.target.value)}
                             fullWidth
                             required
+                            disabled={!time}
+                            error={!!cowGroupError}
+                            helperText={cowGroupError}
                             displayEmpty
                             style={{
                                 marginBottom: '16px'
@@ -142,10 +238,13 @@ const ScheduleFormContent = () => {
                             name="specialNotes"
                             label="Special Notes"
                             value={specialNotes}
-                            onChange={(e) => setSpecialNotes(e.target.value)}
+                            onChange={(e) => handleSpecialNotesChange(e.target.value)}
                             fullWidth
                             multiline
                             rows={6}
+                            disabled={!cowGroup}
+                            error={!!specialNotesError}
+                            helperText={specialNotesError}
                             style={{
                                 marginBottom: '48px'
                             }}
@@ -162,6 +261,7 @@ const ScheduleFormContent = () => {
                                 marginBottom: '22px',
                                 borderRadius: '15px'
                             }}
+                            disabled={!isFormValid()}
                         >
                             Add Milk Session
                         </Button>
