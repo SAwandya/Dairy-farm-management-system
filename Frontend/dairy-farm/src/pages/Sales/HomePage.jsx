48c1fe4b04f4746fe3230c98175d7fda9a7898fb
SAwandya <sachilaawandya@gmail.com>
2024-05-04 21:09:49 +0530

    create footer for homepage

--- Frontend/dairy-farm/src/pages/Sales/HomePage.jsx
+++ Frontend/dairy-farm/src/pages/Sales/HomePage.jsx
@@ -15,10 +15,202 @@ const HomePage = () => {
           height: "100vh",
         }}
       ></Box>
-      <Typography sx={{ fontSize: '40px', fontWeight: 'bold', textAlign: 'center', marginTop: '40px', marginBottom:'20px' }}>
+      <Typography
+        sx={{
+          fontSize: "40px",
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "40px",
+          marginBottom: "20px",
+        }}
+      >
         PRODUCTS
       </Typography>
       <ProductGrid />
+      <Box
+        sx={{
+          height: "40vh",
+          backgroundColor: "#114232",
+          marginTop: "40px",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            padding: "40px",
+            marginRight: "20px"
+          }}
+        >
+          <Box>
+            <Typography
+              fontFamily="serif"
+              fontSize={35}
+              fontWeight={10}
+              color="#87A922"
+            >
+              On the Farm
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Meet Our Farmers
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Meet the Cows
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Famous Dairy Facts
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              From the Farm to the Fridge
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Dairy Farm Map
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Farm Practices
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              fontFamily="serif"
+              fontSize={35}
+              fontWeight={10}
+              color="#87A922"
+            >
+              In the Kitchen
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Recipes
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Lactose Intolerance
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Milk Imitators
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Organic Milk
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Flavored Milk
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Ask Our Dietitian
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              fontFamily="serif"
+              fontSize={35}
+              fontWeight={10}
+              color="#87A922"
+            >
+              About Us
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              FAQ
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Our Board
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Our Staff
+            </Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            ></Typography>
+            <Typography
+              fontFamily="serif"
+              sx={{ marginLeft: "10px" }}
+              color="#E8EFCF"
+              fontSize={18}
+            >
+              Contact Us
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
