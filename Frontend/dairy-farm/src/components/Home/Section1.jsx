891695c4a503e6ed5d58e8d05bb4ae2372da6384
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-05-05 03:56:08 +0530

    Complete Section 1 in home page

---
+++ Frontend/dairy-farm/src/components/Home/Section1.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Container, Box, Typography, Button } from '@mui/material';
+
+function Section1() {
+    return (
+        <Box
+            sx={{
+                display: 'flex'
+            }}
+        >
+            <Box
+                sx={{
+                }}
+            >
+                <Typography
+                    variant='h2'
+                    sx={{
+                        fontFamily: 'Poppins',
+                        fontWeight: '600',
+                        color: '#38775B',
+                        textAlign: 'left',
+                        fontSize: '70px',
+                        width: '70%',
+                        marginTop: '54px'
+                    }}
+                >
+                    Our Passion for Agriculture Nurturing Growth and Sustaining the Future
+                </Typography>
+                <Typography
+                    variant='h6'
+                    sx={{
+                        fontFamily: 'Poppins',
+                        fontWeight: '400',
+                        color: '#000',
+                        textAlign: 'left',
+                        width: '70%',
+                        fontSize: '14px',
+                        marginTop: '20px'
+                    }}
+                >
+                    Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.
+                </Typography>
+                <Button
+                    sx={{
+                        backgroundColor: '#38775B',
+                        color: '#fff',
+                        border: '1px solid #38775B',
+                        marginTop: '20px',
+                        fontFamily: 'Poppins',
+                        paddingRight: '30px',
+                        paddingLeft: '30px'
+                    }}
+                >
+                    Get Started
+                </Button>
+            </Box>
+            <img src="../../../../src/assets/section1.png" alt="cows" width={'500px'} height={'500px'} className='section1-img'/>
+        </Box>
+    );
+}
+
+export default Section1;
--- Frontend/dairy-farm/src/pages/Home.jsx
+++ Frontend/dairy-farm/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container, Box } from '@mui/material';
 import HomeNavigation from '../components/Home/HomeNavigation';
 import HeroSection from '../components/Home/HeroSection';
+import HomeContent from '../components/Home/HomeContent';
 
 function Home() {
     return (
         <Box>
             <HomeNavigation />
             <HeroSection />
+            <HomeContent />
         </Box>
     );
 }
