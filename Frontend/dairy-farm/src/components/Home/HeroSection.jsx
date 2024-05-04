f9da5c33bde2ffedfeaccf1f0b89b231fa02078c
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-05-05 03:25:35 +0530

    Create home page structure and hero section of it

---
+++ Frontend/dairy-farm/src/components/Home/HeroSection.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Container, Box, Typography, Button } from '@mui/material';
+
+function HeroSection() {
+    return (
+        <Box className= "bg-container">
+            <img src='../../../../src/assets/homebg2.png' alt="home background" className='home-bg'/>
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    marginTop: '70px'
+                }}
+            >
+                <Typography
+                    variant='h1'
+                    sx={{
+                        fontFamily: 'Poppins',
+                        fontWeight: '600',
+                        color: '#fff',
+                        textAlign: 'center',
+                        fontSize: '90px'
+                    }}
+                >
+                    The Role of Technology in Revolutionizing Dairy Farm Management
+                </Typography>
+                <Typography
+                    variant='h6'
+                    sx={{
+                        fontFamily: 'Poppins',
+                        fontWeight: '400',
+                        color: '#fff',
+                        textAlign: 'center',
+                        margin: 'auto',
+                        width: '50%',
+                        fontSize: '16px',
+                        marginTop: '60px'
+                    }}
+                >
+                    Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.
+                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        marginTop: '40px'
+                    }}
+                >
+                    <Button
+                        sx={{
+                            backgroundColor: '#38775B',
+                            color: '#fff',
+                            border: '1px solid #38775B',
+                            marginRight: '48px'
+                        }}
+                    >Get Started</Button>
+                    <Button
+                        sx={{
+                            color: '#fff',
+                            border: '1px solid #fff'
+                        }}
+                    >
+                        Learn More
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
+
+export default HeroSection;
---
+++ Frontend/dairy-farm/src/components/Home/HomeNavigation.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Box } from '@mui/material';
+
+function HomeNavigation() {
+    return (
+        <Box
+            sx={{
+                height: '100px',
+                backgroundColor: '#fff',
+                display: 'flex',
+                justifyContent: 'space-between',
+                paddingRight: '100px',
+                paddingLeft: '100px',
+            }}
+        >
+            <img src='../../../../src/assets/sidebar-logo.png' alt="logo" width="180px"></img>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <a className="home-nav-link" href="#">Home</a>
+                <a className="home-nav-link" href="/">Shop</a>
+                <a className="home-nav-link" href="/homeM">Admin Section</a>
+            </Box>
+            
+        </Box>
+    );
+}
+
+export default HomeNavigation;
