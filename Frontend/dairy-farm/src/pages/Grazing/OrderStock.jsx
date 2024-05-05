f917225050c55b2db02e7d92abc61d7db0f2d704
imalkipinto <129598612+imalkipinto@users.noreply.github.com>
2024-05-05 10:04:37 +0530

    stock page changes done,have to connect to the button

--- Frontend/dairy-farm/src/pages/Grazing/OrderStock.jsx
+++ Frontend/dairy-farm/src/pages/Grazing/OrderStock.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReduceStockForm from '../../components/ReduceStockForm';
+import ReduceStockForm from '../../components/Supplier/ReduceStockForm';
 
 function OrderStock() {
   return (
-    <div>
+    
+        <div>
+            <Box className="dashboard-content">
+                <div align='center'></div>
+                <GrazingDate/>
+                <Name/>
+              
+              <Typography> Information regarding the  waste management is recorded here</Typography>
+              
+      </Box>
+            
+        <GrazingSideBar sx={{ position: 'fixed', left: 0, top: 0, height: '100vh' }} />
+        <Container
+            className="main-container"
+            sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+            }}
+        ></Container>
       <h1>Order Stock</h1>
       <ReduceStockForm />
     </div>
